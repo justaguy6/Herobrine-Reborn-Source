fa40f2f4807e8d583ae26ffb0bf3454c5c5c447d
Random people <98900217+justaguy6@users.noreply.github.com>
2023-02-10 12:56:23 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -270,7 +270,7 @@ class StoryMenuState extends MusicBeatState
 		{
 			FlxG.sound.play(Paths.sound('cancelMenu'));
 			movedBack = true;
-			MusicBeatState.switchState(new MainMenuState());
+			MusicBeatState.switchState(new menus.MainMenu());
 		}
 
 		super.update(elapsed);
